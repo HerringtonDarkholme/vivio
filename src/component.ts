59032d6b96fb4c77db3e6502451d19c36cb95f28
Herrington Darkholme <HerringtonDarkholme@users.noreply.github.com>
2016-12-01 16:32:22 +0800

    optimize compile performance

--- src/component.ts
+++ src/component.ts
@@ -17,29 +17,29 @@ export type Handlers<T> = {
   [K in keyof T]: (t: T[K]) => void
 }
 
-export type CompStart<Tag, Comp> = {
-  class(nameHash: {[k: string]: boolean}): CompStart<Tag, Comp>
-  on<T>(this: ComponentEventAux<T>, handlerHash: Handlers<T>): CompStart<Tag, Comp>
-  props<C, K extends keyof C>(this: ComponentPropAux<C, K>, nameHash: Partial<Pick<C, K>>): CompStart<Tag, Comp>
-  nativeOn(handlerHash: {[k: string]: Function}): CompStart<Tag, Comp>
-  domProps(nameHash: {[k: string]: any}): CompStart<Tag, Comp>
+export type CompStart<Tag> = {
+  class(nameHash: {[k: string]: boolean}): CompStart<Tag>
+  on<T>(this: ComponentEventAux<T>, handlerHash: Handlers<T>): CompStart<Tag>
+  props<C, K extends keyof C>(this: ComponentPropAux<C, K>, nameHash: Partial<Pick<C, K>>): CompStart<Tag>
+  nativeOn(handlerHash: {[k: string]: Function}): CompStart<Tag>
+  domProps(nameHash: {[k: string]: any}): CompStart<Tag>
  // 'componentTag': Comp
 } & Tag
 
-export type For<T, Tag, EndTag, Comp> = {
-  for<A>(list: A[], func: (t: A, i: number, h: CompStart<Tag, Comp>) => T): EndTag
+export type For<T, Tag, EndTag> = {
+  for<A>(list: A[], func: (t: A, i: number, h: CompStart<Tag>) => T): EndTag
 } & Tag
 
-export type Common<T, EndTag, Comp> =
-  CompStart<For<T, Interpolate<T>, EndTag, Comp>, Comp>
+export type Common<T, EndTag> =
+  CompStart<For<T, Interpolate<T>, EndTag>>
 
 
 export type CloseC<Parent, End extends string, Comp> =
   Close<Parent, End> & {componentTag: Comp}
 
 export type BIf<Parent, End extends string, Comps, Comp> = {
-  if<Pt>(this: {parent: Pt}, condition: boolean): Common<B<CloseC<WithElse<Pt>, End, Comp>, Comps>, Close<WithElse<Pt>, End>, Comp>
-} & Common<B<CloseC<Parent, End, Comp>, Comps>, Close<Parent, End>, Comp>
+  if<Pt>(this: {parent: Pt}, condition: boolean): Common<B<CloseC<WithElse<Pt>, End, Comp>, Comps>, Close<WithElse<Pt>, End>>
+} & Common<B<CloseC<Parent, End, Comp>, Comps>, Close<Parent, End>>
 
 export type ComponentB<Parent, End extends string, Comp, Comps> =
   Literal<
@@ -47,8 +47,8 @@ export type ComponentB<Parent, End extends string, Comp, Comps> =
   >
 
 export type PIf<Parent, End extends string, Comps, Comp> = {
-  if<Pt>(this: {parent: Pt}, condition: boolean): Common<P<CloseC<WithElse<Pt>, End, Comp>, Comps>, Close<WithElse<Pt>, End>, Comp>
-} & Common<P<CloseC<Parent, End, Comp>, Comps>, Close<Parent, End>, Comp>
+  if<Pt>(this: {parent: Pt}, condition: boolean): Common<P<CloseC<WithElse<Pt>, End, Comp>, Comps>, Close<WithElse<Pt>, End>>
+} & Common<P<CloseC<Parent, End, Comp>, Comps>, Close<Parent, End>>
 
 export type ComponentP<Parent, End extends string, Comp, Comps> =
   Literal<
